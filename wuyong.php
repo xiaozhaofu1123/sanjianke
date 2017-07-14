c18323e747fa8518c13ec9d6de7ca430238379d5
小福php <xiaozhaofu@163.com>
2017-07-14 12:02:34 +0800

    修改

--- wuyong.php
+++ wuyong.php
@@ -4,6 +4,6 @@
  * @Author: anchen
  * @Date:   2017-07-13 21:39:57
  * @Last Modified by:   anchen
- * @Last Modified time: 2017-07-14 11:56:43
+ * @Last Modified time: 2017-07-14 12:01:10
  */
-echo "2017-07-14";
+echo "2017-07-14修改";
